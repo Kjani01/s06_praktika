7ed7b74132b64f0f6e44fd5f3ebdd8bf4c1b51ec
Kjani01 <32632744+Kjani01@users.noreply.github.com>
2019-04-10 14:42:02 +0200

    Update
    Warnings solved with casts

--- EmSys_LiSi/a1.c
+++ EmSys_LiSi/a1.c
@@ -3,9 +3,12 @@
 #include <unistd.h>
 
 void fprintPt(pthread_t pt) {
+
+	size_t i;
+
     unsigned char *ptc = (unsigned char*)(void*)(&pt);
     printf("0x");
-    for (size_t i=0; i<sizeof(pt); i++) {
+    for (i=0; i<sizeof(pt); i++) {
         printf("%02x", (unsigned)(ptc[i]));
     }
 }
@@ -14,38 +17,40 @@ void * threadHello(void * vargp) {
     int st = *(int *)vargp;
     sleep(st);
     printf("Hello from thread %d. My name is: ", st);
-    fprintPt(pthread_self);
+    fprintPt(pthread_self());
     printf("\n");
-    pthread_exit(pthread_self);
+    pthread_exit((void *)pthread_self());
 }
 
 int main() {
   int MAXTHREADS = 2;
-    printf("MAIN THREAD: ");
-    fprintPt(pthread_self);
-    printf("\n");
+    /*printf("MAIN THREAD: ");
+    fprintPt(pthread_self());
+    printf("\n");*/
   printf("Creating threads ...\n");
   void *rv;
 
   pthread_t threads[MAXTHREADS];
 
-  for (int i = 0; i < MAXTHREADS; ++i) {
+  int i;
+
+  for (i = 0; i < MAXTHREADS; ++i) {
       pthread_create(&(threads[i]), NULL, threadHello, &i);
       printf("Creating thread %d: ", i);
       fprintPt(threads[i]);
       printf(".\n");
   }
-  
-  for (int i = 0; i < MAXTHREADS; ++i) {
+
+  for (i = 0; i < MAXTHREADS; ++i) {
       pthread_join(threads[i], &rv);
-      pthread_t tmp = rv;
-      
+      pthread_t tmp = (pthread_t) rv;
+
       printf("Joining thread %d: we called it ", i);
       fprintPt(threads[i]);
       printf(", it called itself ");
       fprintPt(tmp);
       printf(".\n");
-      
+
       if (pthread_equal(tmp, threads[i])) {
           printf("Thread %d had the same ID.\n", i);
       }

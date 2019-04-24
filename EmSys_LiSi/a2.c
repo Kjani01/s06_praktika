0c5847a30d3cff48de3346059c92b32b23a41f6f
pocketinfinity <sthum@hm.edu>
2019-04-24 12:53:31 +0200

    a2 wip for now

--- EmSys_LiSi/a2.c
+++ EmSys_LiSi/a2.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 #include <time.h>
 
+int changeSystemTick(unsigned int microsecs) {
+    printf("SYSTEM CLOCK GRANULARITY %d\n", ticksize)
+    ticksize (microsecs * 1000);
+    printf("SYSTEM CLOCK GRANULARITY %d\n", ticksize)
+    return 0;
+}
+
 int main() {
+    //changeSystemTick(1);
+    
     printf("Der 1ms-Sleep\n.");
     
+    struct timespec current;
     struct timespec deadline;
+    struct timespec aftertime;
     clock_gettime(CLOCK_MONOTONIC, &deadline);
     
-    printf("%lld.%.9ld\n", (long long)deadline.tv_sec, deadline.tv_nsec);
+    deadline = current;
     
-    deadline.tv_nsec += 1000;
+    int i = 0;
+    while (i = 0; i < 1000000; i++) {
+        deadline.tv_nsec += 1000;
+        clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &deadline, NULL);
+    }
     
-    clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &deadline, NULL);
+    clock_gettime(CLOCK_MONOTONIC, &aftertime);
     
-    printf("%lld.%.9ld\n", (long long)deadline.tv_sec, deadline.tv_nsec);
+    printf("       START TIME %lld.%.9ld\n", (long long)current.tv_sec, current.tv_nsec);
+    deadline = current;
+    deadline.tv_sec += 1;
+    printf("PLANNED STOP TIME %lld.%.9ld\n", (long long)deadline.tv_sec, deadline.tv_nsec);
+    printf("   REAL STOP TIME  %lld.%.9ld\n", (long long)aftertime.tv_sec, aftertime.tv_nsec);
     
     return 0;
 }

484e748d53e4e9c8776e6d216884891d17b6b8e9
Kjani01 <lisa_obermaier@gmx.de>
2019-05-09 21:13:41 +0200

    Mockup für Sweng II

---
+++ CGBV/Praktikum 1/Abgabe LiSi/Abgabe1_Obermaier_Thum/FragmentShaderDilatation.glsl
@@ -0,0 +1,81 @@
+//
+// Fragment Shader für den morphologischen Operator 
+// Dilatation
+// Angepasst für Core Profile
+// ---------------------------------
+//
+// @author: Prof. Dr. Alfred Nischwitz
+// @lecturer: Prof. Dr. Alfred Nischwitz
+//
+// (c)2017 Hochschule München, HM
+//
+// ---------------------------------
+#version 330
+
+
+smooth in vec2 texCoords;				// pixelbezogene Texturkoordinate
+out vec4 fragColor;					// Ausgabewert mit 4 Komponenten zwischen 0.0 und 1.0
+uniform sampler2DRect textureMap;		// Sampler für die Texture Map
+uniform vec4 param1;					// Param1 X,Y,Z,W in GUI
+
+uniform vec2 offsets[9] = vec2[](	vec2(-1,  1),
+									vec2(-1,  0),
+									vec2(-1, -1),
+									vec2( 0,  1),
+									vec2( 0,  0),
+									vec2( 0, -1),
+									vec2( 1,  1),
+									vec2( 1,  0),
+									vec2( 1, -1)	);
+
+uniform vec2 offsetsB[25] = vec2[](	vec2(-2,  2), vec2(-2,  1), vec2(-2,  0), vec2(-2,  1), vec2(-2,  2),
+									vec2(-1,  2), vec2(-1,  1), vec2(-1,  0), vec2(-1,  1), vec2(-1,  2),
+									vec2( 0,  2), vec2( 0,  1), vec2( 0,  0), vec2( 0,  1), vec2( 0,  2),
+									vec2( 1,  2), vec2( 1,  1), vec2( 1,  0), vec2( 1,  1), vec2( 1,  2),
+									vec2( 2,  2), vec2( 2,  1), vec2( 2,  0), vec2( 2,  1), vec2( 2,  2)	);
+
+uniform vec2 offsetsC[49] = vec2[](	vec2(-3,  3), vec2(-2,  3), vec2(-1,  3), vec2(0,  3), vec2(1,  3), vec2(2,  3), vec2(3,  3), 
+										vec2(-3,  2), vec2(-2,  2), vec2(-1,  2), vec2(0,  2), vec2(1,  2), vec2(2,  2), vec2(3,  2),
+										vec2(-3,  1), vec2(-2,  1), vec2(-1,  1), vec2(0,  1), vec2(1,  1), vec2(2,  1), vec2(3,  1),
+										vec2(-3,  0), vec2(-2,  0), vec2(-1,  0), vec2(0,  0), vec2(1,  0), vec2(2,  0), vec2(3,  0),
+										vec2(-3, -1), vec2(-2, -1), vec2(-1, -1), vec2(0, -1), vec2(1, -1), vec2(2, -1), vec2(3, -1),
+										vec2(-3, -2), vec2(-2, -2), vec2(-1, -2), vec2(0, -2), vec2(1, -2), vec2(2, -2), vec2(3, -2),
+										vec2(-3, -3), vec2(-2, -3), vec2(-1, -3), vec2(0, -3), vec2(1, -3), vec2(2, -3), vec2(3, -3)	);
+
+void main()
+{
+
+	float window = 3.0;
+	
+	if (param1[2] == 5) {
+		window = 5.0;
+	}
+	else if (param1[2] == 7)  {
+		window = 7.0;
+	}
+
+	vec4 texel = texture(textureMap, texCoords);
+	vec4 max = texel;
+	vec4 tmp;
+
+	for (int i = 0; i < window*window; i++) {
+		
+			if (window == 5.0) {
+				tmp = texture(textureMap, texCoords + offsetsB[i]);
+			}
+			else if (window == 7.0) {
+				tmp = texture(textureMap, texCoords + offsetsC[i]);
+			}
+			else {
+				tmp = texture(textureMap, texCoords + offsets[i]);
+			}
+
+		if (max(tmp, max) == tmp) {
+			max = tmp;
+		}
+	}
+
+
+    fragColor =	max;
+
+}
---
+++ CGBV/Praktikum 1/Abgabe LiSi/Abgabe1_Obermaier_Thum/FragmentShaderGauss3x3.glsl
@@ -0,0 +1,57 @@
+//
+// Fragment Shader für 3x3 Gauss Tiefpass-Filter
+// Angepasst für Core Profile
+// ---------------------------------
+//
+// @author: Prof. Dr. Alfred Nischwitz
+// @lecturer: Prof. Dr. Alfred Nischwitz
+//
+// (c)2017 Hochschule München, HM
+//
+// ---------------------------------
+#version 330
+
+
+smooth in vec2 texCoords;				// pixelbezogene Texturkoordinate
+out vec4 fragColor;					// Ausgabewert mit 4 Komponenten zwischen 0.0 und 1.0
+uniform sampler2DRect textureMap;		// Sampler für die Texture Map
+uniform vec4 param1;					// Param1 X,Y,Z,W in GUI
+
+uniform vec2 offsets[9] = vec2[](	vec2(-1,  1),
+									vec2(-1,  0),
+									vec2(-1, -1),
+									vec2( 0,  1),
+									vec2( 0,  0),
+									vec2( 0, -1),
+									vec2( 1,  1),
+									vec2( 1,  0),
+									vec2( 1, -1)	);
+
+void main()
+{
+
+	vec4 texel;
+	float pi = 3.14159265359;
+	float h;
+	float counter = 0.0 ;
+			float p1 = param1[0] / 100;
+
+
+	if (p1 != 0) {
+		for (int i = 0; i < 9; i++) {
+			float x = offsets[i][0];
+			float y = offsets[i][1];
+
+			h  = (1 / (2 * pi * p1 * p1)) * exp(-(x * x + y * y) / (2 * p1 * p1));
+		
+			texel +=  texture(textureMap, texCoords + offsets[i]) * h;
+			counter += h;
+		}
+			
+		fragColor = texel / counter;
+
+	} else 
+		{
+		fragColor = texture(textureMap, texCoords);
+		}
+}

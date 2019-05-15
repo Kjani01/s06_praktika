30405b7ad98eb7bd195e19d70a634a619a46b8cc
pocketinfinity <36113994+pocketinfinity@users.noreply.github.com>
2019-05-15 15:01:50 +0200

    ausgaben rausnehmen!

--- EmSys_LiSi/a4.c
+++ EmSys_LiSi/a4.c
@@ -6,30 +6,8 @@
 #include <unix.h>
 #include <semaphore.h>
 
-void waste_msecstest() {
 
-	int i;
-	for (i = 0; i < 37750; i++) {
-		double calculate = i/13;
-	}
-}
-
-/*int main() {
-	struct timespec before;
-	struct timespec after;
-
-	int i;
-	//for (i = 0; i < 10; i++) {
-		clock_gettime(CLOCK_REALTIME, &before);
-		waste_msecstest();
-		clock_gettime(CLOCK_REALTIME, &after);
-		long diff = after.tv_nsec - before.tv_nsec;
-		printf("Time difference %ld\n", diff);
-	//}
-
-
-	return 0;
-}*/
+	sem_t ourSem;
 
 void waste_msecs(unsigned int msecs) {
 
@@ -43,78 +21,110 @@ void waste_msecs(unsigned int msecs) {
 
 void * threadSetSemaphore(void * vargp) {
     printf("Hello from Task 1.\n");
-	sem_t ourSem = *(int *)vargp;
 
-	/*pthread_attr_t attrib;
-	struct sched_param param;
-	pthread_attr_init(&attrib);
-	pthread_attr_getschedparam(&attrib, &param);
-	int i = param->sched_priority;
-	printf("My Priority is %d. \n", i);
+	int ret = pthread_setschedprio(pthread_self(), 50);
+	if ( ret != 0 ){
+	   printf ("pthread_setschedprio: %s\n", strerror(ret));
+	   exit(-1);
+	}
 
-	//param->sched_priority= 1;
-	pthread_attr_setschedparam(&attrib, &param);*/
-    
 	int i = 0;
-	
-	while (true) {
+
+	int j;
+	for (j = 0; j < 1000; j++) {
+
 		i++;
+
+		int semv;
+		sem_getvalue(&ourSem, &semv);
+	    printf("> Task 1: sem_t @ %d\n", semv);
+
+		waste_msecs(2);
+		nap(2);
+
 		if (i == 3) {
-			sem_post(&ourSem);
+			ret = sem_post(&ourSem);
+			if ( ret != 0 ){
+							   printf ("sem_post: %s\n", strerror(ret));
+							   exit(-1);
+							}
+			printf("> Task 1: i'm now posting\n");
 			i = 0;
 		}
-		waste_msecs(1);
-		nap(2);
+
+	    printf("Task 1 finished.\n");
+
 	}
-	
+
 	pthread_exit((void *)pthread_self());
 }
 
 void * threadWaitForSemaphore(void * vargp) {
 	printf("Hello from Task 2.\n");
-	sem_t ourSem = *(int *)vargp;
-	
-	while (true) {
-		sem_wait(&ourSem);
+
+	int ret = pthread_setschedprio(pthread_self(), 100);
+		if ( ret != 0 ){
+		   printf ("pthread_setschedprio: %s\n", strerror(ret));
+		   exit(-1);
+		}
+
+		int j;
+	for (j = 0; j < 1000; j++) {
+		int semv;
+		sem_getvalue(&ourSem, &semv);
+	    printf("> Task 2: sem_t @ %d\n", semv);
+
+	    printf("> Task 2: waiting on my sem.\n");
+		ret = sem_wait(&ourSem);
+		if ( ret != 0 ){
+				   printf ("sem_wait: %s\n", strerror(ret));
+				   exit(-1);
+				}
+		printf("> Task 2: sem has been posted, continuing.\n");
 		waste_msecs(3);
+	    printf("Task 2 finished.\n");
 	}
-	
+
 	pthread_exit((void *)pthread_self());
-	
+
 }
 
 int main() {
-	
+
+
+
 	//Semaphore
-	
-	sem_t ourSem;
-	int ret = sem_init( ourSem, 1, unsigned value );
-	
+
+	int ret = sem_init( &ourSem, 0, 1 );
+
 		//error handling
 		if ( ret != 0 ){
 		   printf ("pthread_create: %s\n", strerror(ret));
 		   exit(-1);
 		}
-	
+
 	pthread_t threads[2];
 	//pthread_attr_t attrib;
 
 	//pthread_attr_setinheritsched(&attrib,PTHREAD_EXPLICIT_SCHED );
 	  //create threads for number maxthreads
-		  ret = pthread_create(&(threads[0]), NULL, threadSetSemaphore, &ourSem);
+	pthread_attr_t attrib;
+	pthread_attr_setinheritsched(&attrib,PTHREAD_EXPLICIT_SCHED );
+
+		  ret = pthread_create(&(threads[0]), NULL, threadSetSemaphore, NULL);
 		  //error handling
 		  if ( ret != 0 ){
 			   printf ("pthread_create: %s\n", strerror(ret));
 			   exit(-1);
 		  }
-		  
-		  ret = pthread_create(&(threads[1]), NULL, threadWaitForSemaphore, &ourSem);
+
+		  ret = pthread_create(&(threads[1]), NULL, threadWaitForSemaphore, NULL);
 		  //error handling
 		  if ( ret != 0 ){
 			   printf ("pthread_create: %s\n", strerror(ret));
 			   exit(-1);
 		  }
-	  
+
 	  void *rv;
 
 		  ret = pthread_join(threads[0], &rv);
@@ -123,11 +133,130 @@ int main() {
 			   printf ("pthread_create: %s\n", strerror(ret));
 			   exit(-1);
 		  }
-		  
+
 		 ret = pthread_join(threads[1], &rv);
 		  //error handling
 		  if ( ret != 0 ){
 			   printf ("pthread_create: %s\n", strerror(ret));
 			   exit(-1);
 		  }
+
+		  return 0;
 }
+
+
+//#include <stdio.h>
+//#include <time.h>
+//#include <sys/neutrino.h>
+//#include <pthread.h>
+//#include <sched.h>
+//
+//void waste_msecstest() {
+//
+//	int i;
+//	for (i = 0; i < 37750; i++) {
+//		double calculate = i/13;
+//	}
+//}
+//
+//void waste_msecs(unsigned int msecs) {
+//
+//	int iterations = msecs * 37750;
+//	int i;
+//
+//	for (i = 0; i < iterations; i++) {
+//		double calculate = i/13;
+//	}
+//}
+//
+//void * threadScheduling(void * vargp) {
+//    printf("Hello from thread %d\n", pthread_self());
+//
+//	pthread_attr_t attrib;
+//	struct sched_param param;
+//	pthread_attr_init(&attrib);
+//	//int ret = pthread_attr_getschedparam(&attrib, &param);
+//	int policy;
+//	policy = SCHED_RR;
+//
+//	int ret = pthread_getschedparam(pthread_self(), &policy, &param);
+//	if ( ret != 0 ){
+//					   printf ("pthread_getschedparam: %s\n", strerror(ret));
+//					   exit(-1);
+//				  }
+//	//int i = param->sched_priority;
+//	printf("My Priority is %d. \n", param.sched_priority);
+//
+//	/*param.sched_priority= 100;
+//	ret = pthread_attr_setschedparam(&attrib, &param);
+//	 if ( ret != 0 ){
+//				   printf ("pthread_attr_setschedparam: %s\n", strerror(ret));
+//				   exit(-1);
+//			  }*/
+//
+//	ret = pthread_setschedprio(pthread_self(), 500);
+//	if ( ret != 0 ){
+//						   printf ("pthread_setschedprio: %s\n", strerror(ret));
+//						   exit(-1);
+//					  }
+//
+//    waste_msecs(1);
+//
+//	ret = pthread_getschedparam(pthread_self(), &policy, &param);
+//	if ( ret != 0 ){
+//					   printf ("pthread_getschedparam: %s\n", strerror(ret));
+//					   exit(-1);
+//				  }
+//	printf("My Priority is now %d. \n", param.sched_priority);
+//	pthread_exit((void *)pthread_self());
+//}
+//
+///*int main() {
+//	struct timespec before;
+//	struct timespec after;
+//
+//	int i;
+//	//for (i = 0; i < 10; i++) {
+//		clock_gettime(CLOCK_REALTIME, &before);
+//		waste_msecstest();
+//		clock_gettime(CLOCK_REALTIME, &after);
+//		long diff = after.tv_nsec - before.tv_nsec;
+//		printf("Time difference %ld\n", diff);
+//	//}
+//
+//
+//	return 0;
+//}*/
+//
+//int main() {
+//
+//
+//	pthread_t threads[1];
+//	pthread_attr_t attrib;
+//
+//	pthread_attr_setinheritsched(&attrib,PTHREAD_EXPLICIT_SCHED );
+//	  int i;
+//	  //create threads for number maxthreads
+//	  for (i = 0; i < 1; ++i) {
+//		  int ret = pthread_create(&(threads[i]), NULL, threadScheduling, &i);
+//		  //error handling
+//		  if ( ret != 0 ){
+//			   printf ("pthread_create: %s\n", strerror(ret));
+//			   exit(-1);
+//		  }
+//	  }
+//
+//	  void *rv;
+//
+//	  for (i = 0; i < 1; ++i) {
+//		  int ret = pthread_join(threads[i], &rv);
+//
+//		  //error handling
+//		  if ( ret != 0 ){
+//			   printf ("pthread_create: %s\n", strerror(ret));
+//			   exit(-1);
+//		  }
+//	  }
+//
+//	return 0;
+//}

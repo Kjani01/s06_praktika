babcd08396c2758402b8c3ce5cc0920e353afb90
Kjani01 <32632744+Kjani01@users.noreply.github.com>
2019-05-29 14:57:20 +0200

    waste_msecs funktioniert jetzt wie es soll

--- EmSys_LiSi/a4.c
+++ EmSys_LiSi/a4.c
@@ -51,26 +51,22 @@ void waste_msecstest() {
 //	return 0;
 //}
 
+
+void sleeping();
+
 void waste_msecs(unsigned int msecs) {
 
-	long iterations = msecs * 15300;
+	long iterations = msecs * 20000;
 	long i;
-
+	volatile double calculate;
 	for (i = 0; i < iterations; i++) {
-		double calculate = i/13*(i-1);
+		calculate = i/13*(i+1);
 	}
 	//printf("Ende Waste Msecs, Iterations: %ld\n", iterations);
+	calculate++;
 }
 
-void waste_msecs2(unsigned int msecs) {
 
-	long iterations = msecs * 32300;
-	long i;
-
-	for (i = 0; i < iterations; i++) {
-		double calculate = i/13*(i-1);
-	}
-}
 
 void * threadSetSemaphore(void * vargp) {
     //printf("Hello from Task 1.\n");
@@ -88,25 +84,35 @@ void * threadSetSemaphore(void * vargp) {
 
 		i++;
 
-		int semv;
-		sem_getvalue(&ourSem, &semv);
+		//int semv;
+		//sem_getvalue(&ourSem, &semv);
 	    //printf("> Task 1: sem_t @ %d\n", semv);
 
 		waste_msecs(2);
-		nap(2);
+		//nap(2);
+		//sleeping();
+
+	    struct timespec deadline;
+	    //clock_gettime(CLOCK_REALTIME, &deadline);
+	    deadline.tv_nsec = 2000000;
+
+	    int errorCode = clock_nanosleep(CLOCK_REALTIME, 0, &deadline, NULL);
+		if (errorCode != 0)
+		{
+			printf("Error in Nanosleep: %s\n", strerror(errorCode));
+			continue;
+		}
 
 		if (i == 3) {
-							ret = sem_post(&ourSem);
-							if ( ret != 0 ){
-											   //printf ("sem_post: %s\n", strerror(ret));
-											   exit(-1);
-											}
-							//printf("> Task 1: i'm now posting\n");
-							i = 0;
-						}
-
+		ret = sem_post(&ourSem);
+		if ( ret != 0 ){
+		   printf ("sem_post: %s\n", strerror(ret));
+		   exit(-1);
+		}
+		//printf("> Task 1: i'm now posting\n");
+		i = 0;
+		}
 	    //printf("Task 1 finished.\n");
-
 	}
 
 	pthread_exit((void *)pthread_self());
@@ -123,8 +129,8 @@ void * threadWaitForSemaphore(void * vargp) {
 
 		int j;
 	for (j = 0; j < 1000; j++) {
-		int semv;
-		sem_getvalue(&ourSem, &semv);
+		//int semv;
+		//sem_getvalue(&ourSem, &semv);
 	    //printf("> Task 2: sem_t @ %d\n", semv);
 
 	    //printf("> Task 2: waiting on my sem.\n");
@@ -135,7 +141,8 @@ void * threadWaitForSemaphore(void * vargp) {
 				}
 		//printf("> Task 2: sem has been posted, continuing.\n");
 		//nap(2);
-		waste_msecs2(3);
+		//waste_msecs2(3);
+		waste_msecs(3);
 	    //printf("Task 2 finished.\n");
 	}
 
@@ -158,12 +165,9 @@ int main() {
 		}
 
 	pthread_t threads[2];
-	//pthread_attr_t attrib;
 
+	//pthread_attr_t attrib;
 	//pthread_attr_setinheritsched(&attrib,PTHREAD_EXPLICIT_SCHED );
-	  //create threads for number maxthreads
-	pthread_attr_t attrib;
-	pthread_attr_setinheritsched(&attrib,PTHREAD_EXPLICIT_SCHED );
 
 		  ret = pthread_create(&(threads[0]), NULL, threadSetSemaphore, NULL);
 		  //error handling
@@ -184,20 +188,59 @@ int main() {
 		  ret = pthread_join(threads[0], &rv);
 		  //error handling
 		  if ( ret != 0 ){
-			   printf ("pthread_create: %s\n", strerror(ret));
+			   printf ("pthread_join: %s\n", strerror(ret));
 			   exit(-1);
 		  }
 
 		 ret = pthread_join(threads[1], &rv);
 		  //error handling
 		  if ( ret != 0 ){
-			   printf ("pthread_create: %s\n", strerror(ret));
+			   printf ("pthread_join: %s\n", strerror(ret));
 			   exit(-1);
 		  }
 
 		  return 0;
 }
 
+
+/*void sleeping() {
+
+	    struct timespec current;
+	    struct timespec deadline;
+
+	    clock_gettime(CLOCK_REALTIME, &current);
+	    clock_gettime(CLOCK_REALTIME, &deadline);
+
+	    deadline.tv_nsec += 2000000;
+
+	    unsigned long i = 0;
+
+	    while (!(current.tv_sec >= deadline.tv_sec && current.tv_nsec >= deadline.tv_nsec)) {
+
+			//Wir müssen noch eine Sekunde drauf rechnen!
+
+			long sec = aftertime.tv_sec;
+			long nsec = aftertime.tv_nsec + 1000000; // + 1 ms
+			// 10^(-9) nsec = 1 sec -> auf sec draufrechnen
+			if (nsec >= 1000000000) {
+				sec += nsec / 1000000000;
+				nsec %= 1000000000;
+			}
+			aftertime.tv_sec = sec;
+			aftertime.tv_nsec = nsec;
+
+			int errorCode = clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &aftertime, NULL);
+	        // printf("       START TIME %lld.%.9ld\n", (long long)current.tv_sec, current.tv_nsec);
+	        if (errorCode != 0)
+			{
+				//printf("Error in Nanosleep: %s\n", strerror(errorCode));
+				continue;
+			}
+
+			i++;
+			clock_gettime(CLOCK_REALTIME, &current);
+	    }
+}*/
 
 //#include <stdio.h>
 //#include <time.h>
